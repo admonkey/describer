ebed0a2e21fbc43e2411472e36e80b18159c3b08
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-01 10:40:59 -0500

    add missing node

---
+++ tests/data/sample.expected.php
@@ -0,0 +1,52 @@
+<?php
+
+$result = [
+
+  "Data" => [
+    "max_count" => 1,
+    "elements" => [
+
+      "Record" => [
+        "max_count" => 2,
+        "elements" => [
+
+          "SAMPLE" => [
+            "max_count" => 2,
+            "elements" => [
+
+              "TITLE" => [
+                "max_count" => 1
+              ],
+              "SUBTITLE" => [
+                "max_count" => 1
+              ],
+              "AUTH" => [
+                "max_count" => 2,
+                "elements" => [
+
+                  "FNAME" => [
+                    "max_count" => 1
+                  ],
+                  "DISPLAY" => [
+                    "max_count" => 1
+                  ]
+
+                ]
+              ],
+              "ABSTRACT" => [
+                "max_count" => 1
+              ]
+
+            ]
+          ]
+
+        ]
+      ]
+
+    ]
+  ]
+
+];
+
+// print_r($result);
+print(serialize($result));
