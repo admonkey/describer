f964e3e60ca7cbcaf215194e84f314ba93c6e521
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-01 15:01:42 -0500

    use local variable

--- src/Describer.php
+++ src/Describer.php
@@ -11,16 +11,28 @@ class Describer
   }
 
   private function countArrayElements(&$array, &$result){
+
+    $local_count = [];
+
     // get collection of subnodes
     foreach ($array as $node){
 
       $name = $this->stripNamespace($node['name']);
 
       // get count of distinct subnodes
-      if (empty($result[$name])){
-        $result[$name]["max_count"] = 1;
+      if (empty($local_count[$name]["max_count"])){
+        $local_count[$name]["max_count"] = 1;
       } else {
-        $result[$name]["max_count"]++;
+        $local_count[$name]["max_count"]++;
+      }
+
+      if(empty($result[$name]["max_count"])){
+        $result[$name]["max_count"] = $local_count[$name]["max_count"];
+      } else {
+        $result[$name]["max_count"] = max(
+          $local_count[$name]["max_count"],
+          $result[$name]["max_count"]
+        );
       }
 
       if (is_array($node['value'])){
